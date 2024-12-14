069afc21aca92c428a85994370f94344ff0f4875
Linus Wallin <linuswallin@live.se>
2024-12-14 10:56:57 +0100

    feat: adds classes and repositories

---
+++ app/src/main/java/com/example/commandercounter/data/models/Player.kt
@@ -0,0 +1,8 @@
+package com.example.commandercounter.data.models
+
+data class Player(
+    val name: Int,
+    var lifeTotal: Int
+) {
+
+}
---
+++ app/src/main/java/com/example/commandercounter/data/repository/GameRepository.kt
@@ -0,0 +1,4 @@
+package com.example.commandercounter.data.repository
+
+class GameRepository {
+}
